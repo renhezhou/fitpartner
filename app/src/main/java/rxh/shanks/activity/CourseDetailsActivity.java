b6754cf461c7e678f974aaba5b353dd97ecfc61e
任河洲 <577119371@qq.com>
2016-08-19 10:52:16 +0800

    测试下先

--- app/src/main/java/rxh/shanks/activity/CourseDetailsActivity.java
+++ app/src/main/java/rxh/shanks/activity/CourseDetailsActivity.java
@@ -3,10 +3,12 @@ package rxh.shanks.activity;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -17,12 +19,11 @@ import butterknife.ButterKnife;
 import rxh.shanks.adapter.CourseDetailsPageAdapter;
 import rxh.shanks.base.BaseActivity;
 import rxh.shanks.fragment.CalendarFragment;
-import rxh.shanks.fragment.CourseDetailsFragment;
 import rxh.shanks.utils.CreatTime;
 
 /**
  * Created by Administrator on 2016/8/3.
- * 课程详情。包括团体课和免费课程
+ * 团体课
  */
 public class CourseDetailsActivity extends BaseActivity implements CalendarFragment.CalendarFragmentListener {
 
@@ -38,16 +39,16 @@ public class CourseDetailsActivity extends BaseActivity implements CalendarFragm
     TextView club_name;
     @Bind(R.id.viewpager)
     ViewPager viewpager;
-    String clubname, flag;//1代表免费课程，2代表团体课
+    String clubname;
 
     CalendarFragment calendarFragment;
     CourseDetailsPageAdapter courseDetailsPageAdapter;
     private List<String> datelist = new ArrayList<>();
+    private List<String> seclet = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        flag = getIntent().getStringExtra("flag");
         clubname = getIntent().getStringExtra("club_name");
         initview();
     }
@@ -55,16 +56,13 @@ public class CourseDetailsActivity extends BaseActivity implements CalendarFragm
     public void initview() {
         setContentView(R.layout.activity_course_details);
         ButterKnife.bind(this);
-        if (flag.equals("1")) {
-            title.setText("免费课程");
-        } else {
-            title.setText("团体课");
-        }
+        title.setText("团体课");
         club_name.setText(clubname);
         back.setOnClickListener(this);
         calendar.setOnClickListener(this);
         datelist = CreatTime.creat(new Date());
-        courseDetailsPageAdapter = new CourseDetailsPageAdapter(getSupportFragmentManager(), this, datelist, flag);
+        seclet = CreatTime.seclet(new Date());
+        courseDetailsPageAdapter = new CourseDetailsPageAdapter(getSupportFragmentManager(), this, datelist, seclet);
         viewpager.setAdapter(courseDetailsPageAdapter);
         tab_layout.setupWithViewPager(viewpager);
         tab_layout.setTabMode(TabLayout.MODE_FIXED);
@@ -95,8 +93,10 @@ public class CourseDetailsActivity extends BaseActivity implements CalendarFragm
     @Override
     public void checkdate(String date) {
         datelist.clear();
+        seclet.clear();
         datelist = CreatTime.creat(CreatTime.conversion(date));
-        courseDetailsPageAdapter = new CourseDetailsPageAdapter(getSupportFragmentManager(), this, datelist, flag);
+        seclet = CreatTime.seclet(CreatTime.conversion(date));
+        courseDetailsPageAdapter = new CourseDetailsPageAdapter(getSupportFragmentManager(), this, datelist, seclet);
         viewpager.setAdapter(courseDetailsPageAdapter);
     }
 
